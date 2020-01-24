e96f5c38ca29deb8d871ffa1c811707414fc3d65
Dan Boykis <danboykis@gmail.com>
2020-01-24 13:08:26 -0500

    0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject com.danboykis/abk "0.1.2"
+(defproject com.danboykis/abk "0.1.3-SNAPSHOT"
   :description "One atom state management for clojure apps"
   :url "http://github.com/danboykis/abk"
   :license {:name "Unlicense"
             :url "http://unlicense.org/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :deploy-repositories [["releases" {:url "https://repo.clojars.org" :sign-releases false}]
+                        ["snapshots" :clojars]])
